8af2ba3ef801a6f332d64a927539a2130aca9ba9
Sumeet Bansal <sumeetbansal@gmail.com>
2017-09-19 08:14:56 -0700

    updated JUnit dependency and ZooKeeper paths to latest versions

--- scripts/zk-wrapper.sh
+++ scripts/zk-wrapper.sh
@@ -2,13 +2,12 @@
 
 declare -A IPTable
 
-IPTable[jeremy]=
-IPTable[steve]=
-IPTable[praneeth]=
-IPTable[chris]=
+IPTable[dev1]=127.0.0.1
+IPTable[dev2]=127.0.0.2
+IPTable[dev3]=127.0.0.3
 
 mkdir root &> /dev/null
 for IP in "${!IPTable[@]}"; do
 	echo $@
-	java -jar zk-crawler.jar generate ${IPTable[$IP]} /alcatrazproperties/2.5 `pwd`/root $IP $@
+	java -jar zk-crawler.jar generate ${IPTable[$IP]} /alcatrazproperties/3.0 `pwd`/root $IP $@
 done
